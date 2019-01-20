9d45beaa3160d6afc141fad01c2dca01b416b913
Anna Springfield <anna.e.springfield@gmail.com>
2019-01-19 22:03:58 -0600

    Remove unnecessary code

--- background.js
+++ background.js
@@ -1,43 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostContains: 'ncaa.com'},
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostContains: 'google.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
     });
-
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostContains: 'ncaa.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
-  });
-
-
-// chrome.runtime.onInstalled.addListener(function() {
-//     // chrome.storage.sync.set({color: '#3aa757'}, function() {
-//     //   console.log("The color is green.");
-//     // });
-//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//         chrome.declarativeContent.onPageChanged.addRules([{
-//           conditions: [new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {hostEquals: 'developer.chrome.com'},
-//           })
-//           ],
-//               actions: [new chrome.declarativeContent.ShowPageAction()]
-//         }]);
-//       });
-// });
-
-// function checkIfOnPage() {
-//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//         chrome.declarativeContent.onPageChanged.addRules([{
-//           conditions: [new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {hostEquals: 'developer.chrome.com'},
-//           })
-//           ],
-//               actions: [new chrome.declarativeContent.ShowPageAction()]
-//         }]);
-//       });
-// }
+});
